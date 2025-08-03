aa993fa6a3daf94fe7a844712e0039bebe7b3c20
ankit1824 <161712067+ankit1824@users.noreply.github.com>
2025-08-03 20:06:14 +0000

    datatypes

---
+++ basics/01_vairables.js
@@ -0,0 +1,14 @@
+const accountId = 14452
+let accountEmail = "akprajapt@gmail.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+
+// accountId = 5    # not allowed
+accountEmail = "ankitp.1824@gmail.com"
+accountPassword = "121212"
+accountCity = "Bengaluru"
+let accountState
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+Prefer not to use var because of issue in block scope and functional scope
+*/
---
+++ basics/02_datTypes.js
@@ -0,0 +1,22 @@
+"use strict"; // treat all JS code as newer versions
+
+// alert(3+3) # we are using node.js, not browser
+
+let name = "Ankit"
+let age = 18
+let isLoggedIn = false
+
+// number => 2 to the power 53
+// bigint
+// string = ""
+// boolean => true/false
+// null => standalone value
+// undefined => value is not yet assigned
+// symbol => uniqueness
+
+
+// object
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
